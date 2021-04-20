349ffc44054a9e826bb4d1e02246d59cd192134e
ju <ju@DESKTOP-602EGTF>
2021-04-20 10:11:29 +0900

    2021.04.20 1. 프로필 변경하는 경로 지정함 2. 상단 프로필 변경하는 경로 지정함    => 프로필, 상단 프로필 변경 됨 3. me페이지에서 글 작성해도 출력되지 않음.

--- src/com/jjj/comment/CommentsDAO.java
+++ src/com/jjj/comment/CommentsDAO.java
@@ -13,103 +13,112 @@ import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
 public class CommentsDAO {
-	public Connection getConn() {
-		String url = "jdbc:oracle:thin:@localhost:1521:xe";
-		String user = "c##jjj";
-		String pass = "jjj123";
-		Connection conn = null;
-		try {
-			Class.forName("oracle.jdbc.driver.OracleDriver");
-			conn = DriverManager.getConnection(url, user, pass);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+   public Connection getConn() {
+      String url = "jdbc:oracle:thin:@localhost:1521:xe";
+      String user = "c##jjj";
+      String pass = "jjj123";
+      Connection conn = null;
+      try {
+         Class.forName("oracle.jdbc.driver.OracleDriver");
+         conn = DriverManager.getConnection(url, user, pass);
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
 
-		return conn;
-	}
+      return conn;
+   }
 
-	public int getAI(Connection conn, String tn) {
-		//�뙎湲�踰덊샇 �옄�룞利앷� �깮�꽦 硫붿냼�뱶
-		String sql = "select nvl(max(commentsid), 0)+1 from " + tn;
-		int maxNum = 0;
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(sql);
+   public int getAI(Connection conn, String tn) {
+      // 뙎湲 踰덊샇  옄 룞利앷   깮 꽦 硫붿냼 뱶
+      String sql = "select nvl(max(commentsid), 0)+1 from " + tn;
+      int maxNum = 0;
+      try {
+         PreparedStatement pstmt = conn.prepareStatement(sql);
 
-			ResultSet rs = pstmt.executeQuery();
-			if (rs.next())
-				maxNum = rs.getInt(1);
-			rs.close();
-			pstmt.close();
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return maxNum;
-	}
+         ResultSet rs = pstmt.executeQuery();
+         if (rs.next())
+            maxNum = rs.getInt(1);
+         rs.close();
+         pstmt.close();
+      } catch (SQLException e) {
+         e.printStackTrace();
+      }
+      return maxNum;
+   }
 
-	public int Insert(Connection conn, Comments comments) {
-		//�뙎湲� �옉�꽦�떆 db���옣 �븯�뒗 硫붿냼�뱶
-		String sql = "INSERT INTO comments (commentsID, contentsID, myMemberID, reply, regTime) " +
-		"VALUES (?, ?, ?, ?, ?)";
+   public int Insert(Connection conn, Comments comments) {
+      // 뙎湲   옉 꽦 떆 db   옣  븯 뒗 硫붿냼 뱶
+      String sql = "INSERT INTO comments (commentsID, contentsID, myMemberID, reply, regTime) " +
+      "VALUES (?, ?, ?, ?, ?)";
 
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			
-			pstmt.setInt(1, comments.getCommentsid()); 
-			pstmt.setInt(2, comments.getContentsid()); 
-			pstmt.setInt(3,	comments.getMymemberid()); 
-			pstmt.setString(4, comments.getReply());
-			pstmt.setString(5, comments.getRegtime());
-			
-			pstmt.executeUpdate();
-			pstmt.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		return 0;
-	}
+      try {
+         PreparedStatement pstmt = conn.prepareStatement(sql);
+         
+         pstmt.setInt(1, comments.getCommentsid()); 
+         pstmt.setInt(2, comments.getContentsid()); 
+         pstmt.setInt(3,   comments.getMymemberid()); 
+         pstmt.setString(4, comments.getReply());
+         pstmt.setString(5, comments.getRegtime());
+         
+         pstmt.executeUpdate();
+         pstmt.close();
+      } catch (SQLException e) {
+         // TODO Auto-generated catch block
+         e.printStackTrace();
+      }
+      
+      return 0;
+   }
 
-	  public Comments getComments(HttpServletRequest request, String nowdate, String commentcontents, int contentsid, int myMemberID) {
-		  //comments (DTO) 蹂��닔�븷 set硫붿냼�뱶 �궗�슜�빐�꽌 媛� 吏��젙�빐二쇰뒗 硫붿냼�뱶
-	Comments comments =	  new Comments();
-	comments.setCommentsid(getAI(getConn(), "Comments"));
-	  comments.setContentsid(contentsid);
-	  comments.setMymemberid(myMemberID);
-	  comments.setReply(commentcontents);
-	  comments.setRegtime(nowdate);
-	  
-	  return comments; 
-	  }
-	  
-	public List<Comments> getCommentsList(Connection conn, int contentsid){
-		// �뙎湲�紐⑸줉 異쒕젰�븯�뒗 硫붿냼�뱶 (List濡� 諛섑솚�떆�궡)
-		String sql  ="select commentsid, contentsid,mymemberid,regtime,reply from comments "
-				+ "where contentsid = ?";
-		List<Comments> lst = new ArrayList<Comments>();
+   public Comments getComments(HttpServletRequest request, String nowdate, String reply, int contentsid, int myMemberID) {
+      System.out.println("용용");
+   Comments comments =   new Comments();
+   comments.setCommentsid(getAI(getConn(), "Comments"));
+   comments.setContentsid(contentsid);
+      System.out.println(contentsid);
+   comments.setMymemberid(myMemberID);
+      System.out.println(myMemberID);
+   comments.setReply(reply);
+   System.out.println(reply);
+   comments.setRegtime(nowdate);
+   System.out.println(nowdate);
+     
+   return comments; 
+   }
+   
+      public List<Comments> getCommentsList(Connection conn, int contentsNo){
+         //  뙎湲 紐⑸줉 異쒕젰 븯 뒗 硫붿냼 뱶 (List濡  諛섑솚 떆 궡)
+         //String sql  ="select m.commentsid,m.mymemberid,m.reply,m.regtime,m.contentsid "
+         //      + "from contents n, comments m ";
+         
+         
+         String sql="select m.commentsid,m.mymemberid,m.reply,m.regtime,m.contentsid "
+               + "from contents n, comments m "
+               + "where n.contentsid=m.contentsid "
+               + "and n.contentsid=?";
+         List<Comments> lst = new ArrayList<Comments>();
 
-		try {
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			pstmt.setInt(1, contentsid);
-			
-			ResultSet rs = pstmt.executeQuery();
-			while(rs.next()) {
-				Comments comments = new Comments();
-				comments.setCommentsid(rs.getInt(1));
-				comments.setContentsid(rs.getInt(2));
-				comments.setMymemberid(rs.getInt(3));
-				comments.setRegtime(rs.getString(4));
-				comments.setReply(rs.getString(5));
-				lst.add(comments);
-				
-			}
+         try {
+            PreparedStatement pstmt = conn.prepareStatement(sql);
+            pstmt.setInt(1, contentsNo);
+            ResultSet rs = pstmt.executeQuery();
+            while(rs.next()) {
+               Comments comments = new Comments();
+               comments.setCommentsid(rs.getInt(1));
+               comments.setMymemberid(rs.getInt(2));
+               comments.setReply(rs.getString(3));
+               comments.setRegtime(rs.getString(4));
+               comments.setContentsid(rs.getInt(5));
+               lst.add(comments);
+               
+            }
 
-			rs.close();
-			pstmt.close();
-		} catch (SQLException e) {			e.printStackTrace();		}
-		
-		System.out.println(lst.size());
-		return lst;
-	}
-
+            rs.close();
+            pstmt.close();
+         } catch (SQLException e) {         e.printStackTrace();      }
+         
+         return lst;
+      }
+      
+      
 }
